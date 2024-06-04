32a6f479782ed72719bae3e21842a1ebc6b6b836
thaleson <thaleson19@hotmail.com>
2024-06-04 13:28:41 -0300

    Fix:add "Data base"

--- PicPhoto/__init__.py
+++ PicPhoto/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import  LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"]="7d443106a96cfc3c4cf728ca949a3498"
 app.config["UPLOAD_FOLDER"]="static/photos_posts"
 
